d6f09b7679536012278eb2e72655493d54ce7739
Vladislav Kharin <vladkharin19@gmail.com>
2022-10-12 13:12:52 +0300

    profile

--- backend/src/main/java/com/vtb/payandsave/controller/ProfileController.java
+++ backend/src/main/java/com/vtb/payandsave/controller/ProfileController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/profile")
 @CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
+//
 public class ProfileController {
 
     @Autowired
