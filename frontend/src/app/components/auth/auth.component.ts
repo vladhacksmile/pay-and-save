712b48fbddb8ee181e4b1dc90658af14189da367
Ibadullaev_Ali <ali.ibadullayev.02@gmail.com>
2022-10-15 13:09:41 +0300

    some fixes

--- frontend/src/app/components/auth/auth.component.ts
+++ frontend/src/app/components/auth/auth.component.ts
@@ -8,7 +8,7 @@ import {Form, FormBuilder, FormGroup} from "@angular/forms";
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.css']
+  styleUrls: ['./auth.component.scss']
 })
 export class AuthComponent implements OnInit {
   form!: FormGroup;
--- frontend/src/app/components/register/register.component.ts
+++ frontend/src/app/components/register/register.component.ts
@@ -6,7 +6,7 @@ import {FormBuilder, FormGroup} from "@angular/forms";
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
   signupInfo: SignUpInfo | undefined;
